729d2345c9d2f7d738e3d96f6bdfb69ed8ea3a1c
Cameron <cmori007@ucr.edu>
2018-11-19 09:57:44 -0800

    GBN format for test case 2

--- client2.py
+++ client2.py
@@ -1,5 +1,4 @@
-import socket, sys, select, time
-from check import ip_checksum
+import socket, sys, time, select	#for sockets
 
 # create dgram udp socket
 try:
@@ -11,39 +10,62 @@ except socket.error:
 host = 'localhost';
 port = 8888;
 
-packet = 0
-numInput = 0
+currLeft = 0		#Current left bound index of the window
+window = 4
+num = 0
+received = 0
+packets = ['Test 1', 'Test 2', 'Test 3', 'Test 4', 'Test 5', 'Test 6', 'Test 7', 'Test 8']
 
-while(1):
-	msg = raw_input('Enter Message to send : ')
+# for packet in packets:
+# 	msg = packet
+# 	if currACK == 1:
+# 		checksum = 'hi'
+# 	else:
+# 		checksum = ip_checksum(msg)
+# 	currACK += 1
+
+# 	try:
+# 		s.sendto(str())
+
+while(1) :
+	if num == 0:
+		for i in range(currLeft, window):
+			msg = packets[i]
+			# checksum = ip_checksum(msg)
+			try:
+				s.sendto(msg, (host, port))
+				print('Sending packet: ' + str(i + 1))
+				num += 1
+				if i == 0:		#Set timeout on first packet sent
+					s.settimeout(10)
+			except socket.error, msg:
+				print 'Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+				sys.exit()
 	
-	#numInput == 1 corrupts the checksum
-	if numInput == 1:
-		checkSum = ''
-	else:
-		checkSum = ip_checksum(msg)
-	numInput = numInput + 1
-	
-    # Send the packet to the server: data = [ACK + msg + checksum]
-	try:
-		s.sendto(str(packet) + msg + checkSum, (host, port))
-		s.settimeout(10)
-		#Receive message from server
+	if received < 8:
 		try:
 			d = s.recvfrom(1024)
+			received += 1
 			reply = d[0]
 			addr = d[1]
+			print(reply)
+
+			# FIXME Implement move window over, send new data, and reset timeout
+			currLeft += 1
+			if currLeft <= len(packets) - window:
+				#Window does not move
+				msg = packets[currLeft + window - 1]
+			else:
+				print(currLeft)
+			if num < 8:
+				s.sendto(msg, (host, port))
+				print(msg)
+				num += 1
 		except socket.timeout:
-			checksum = ip_checksum(msg)
-		#if the timeout happens, send the correct packet to the server again
-			s.sendto(str(packet) + msg + checksum, (host,port))
-			d = s.recvfrom(1024)
-			reply = d[0]
-			addr = d[1]
-
-		print 'Server message : ' + reply
-		packet = 1 - packet #Alternate 1 & 0
-
-	except socket.error, msg:
-		print 'Error Code : ' + str(msg[0]) + 'Message ' + msg[1]
-		sys.exit()
+			print 'Timeout occurred. Resending all packets in window.'
+			print 'Current window: ' + str(currLeft) + '-' + str(currLeft + window)
+			for i in range(currLeft, window):
+				msg = packets[i]
+			# FIXME resend all packets in current window
+	else:
+		break
--- server2.py
+++ server2.py
@@ -1,50 +1,45 @@
-import socket, sys, random, select, time
-from check import ip_checksum
+import socket
+import sys
 
 HOST = ''
 PORT = 8888
 
 try:
-	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	print 'Socket Created'
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    print 'Socket created'
 except socket.error, msg:
-	print 'failed to create socket. Error Code : '+ str(msg[0]) + ' Message ' + msg[1]
-	sys.exit()
+    print 'Failed to create socket. Error code: ' + str(msg[0]) + ' Message ' + str(msg[1])
+    sys.exit()
 
 try:
-	s.bind((HOST,PORT))
+    s.bind((HOST, PORT))
 except socket.error, msg:
-	print 'Bind Failed. Error Code : ' + str(msg[0]) + ' Message ' + msg([1])
-	sys.exit()
+    print 'Bind failed. Error code : ' + str(msg[0]) + ' Message ' + str(msg[1])
+    sys.exit()
 
 print 'Socket bind complete.'
 
-inputs = [s]
-outputs = []
-timeout = 5
-currACK = 0
+currACK = 1
 
 while 1:
+    # receive data from the client (data, addr)
     d = s.recvfrom(1024)
     data = d[0]
     addr = d[1]
-    checksum = ip_checksum(data[1:-2]) # Uses only the raw string of characters
 
     if not data:
         break
-    if data[0] != str(currACK):
-        # Repeated ACK
-        print 'Duplicate.'
-        continue
-    if (data != (str(currACK) + data[1:-2] + checksum)) and (data != (str(not currACK) + data[1:-2] + checksum)):
-        # Checksum is corrupted
-        print 'Corrupted data.'
-        continue
     
-    reply = 'ACK' + str(currACK) + ' Message:' + data[1:-2]
-    currACK = 1 - currACK # 1-1=0 1-0=1
-
+    reply = 'ACK' + str(currACK) + ': ' + data
+    currACK += 1
+    # if currACK == 0:
+    #     reply = 'ACK0: OK...' + data
+    #     currACK = 1
+    # else:
+    #     reply = 'ACK1: OK...' + data
+    #     currACK = 0
+        
     s.sendto(reply, addr)
-    print 'Message [' + addr[0] + ':' + str(addr[1]) + '] - ' + data [1:-2]
+    print 'Message[' + addr[0] + ':' + str(addr[1]) + '] - ' + data.strip()
 
 s.close()
